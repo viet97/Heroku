190d58fd08df0a7b5cb93a6cb396c0522ad6078d
DESKTOP-05I134S\Quoc Viet Dang <quocvietdang97@email.com>
2017-03-09 21:23:39 -0800

    HomeWork13

--- resources/task_resource.py
+++ resources/task_resource.py
@@ -22,7 +22,29 @@ class TaskListRes(Resource):
         added_task = Task.objects().with_id(task.id)
         return  mlab.item2json(added_task)
 
+
+
+
 class TaskRes(Resource):
     def get(self,task_id):
         task = Task.objects().with_id(task_id)
         return mlab.item2json(task)
+
+    def put(self,task_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument(name="name", type=str, location="json")
+        parser.add_argument(name="local_id", type=str, location="json")
+        parser.add_argument(name="done", type=bool, location="json")
+        body = parser.parse_args()
+
+        name = body["name"]
+        local_id = body["local_id"]
+        done = body["done"]
+        task = Task.objects().with_id(task_id)
+        task.update(set__name=name, set__local_id=local_id, set__done=done)
+        return {"message": "ok"}
+
+    def delete(self,task_id):
+        task = Task.objects().with_id(task_id)
+        task.delete()
+        return  {"message":"ok"}
